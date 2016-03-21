9d60f0a62da247126d6331652fe6a459628a17be
邱佳文 <409745930@qq.com>
2016-03-21 15:45:23 +0800

    增加tools.js

--- app/crawler.js
+++ app/crawler.js
@@ -1,35 +1,53 @@
-var querystring = require('querystring'),
-	http = require('http');
+var querystring = require('querystring');
+var http = require('http');
+var crawlConfig = require('../config/crawlConfig.js');
+var myDb = require('./db.js');
+
+crawlConfig.forEach((config, index) => {
+
+})
+
 
 var postData = querystring.stringify({
-	'page': 1
+	'page': 1 //todo1
 });
 
 var options = {
-	hostname: 'coupon.m.jd.com',
-	// host: '127.0.0.1',
+	hostname: 'coupon.m.jd.com', //todo2
+	// host: '127.0.0.1', //走本地代理
 	port: 80,
-	path: '/center/getCouponList.json',
-	method: 'POST',
-	headers: {
+	path: '/center/getCouponList.json', //todo3
+	method: 'POST', //todo4
+	headers: { //todo5
 		'Content-Type': 'application/x-www-form-urlencoded',
 		'Content-Length': postData.length,
 		'Host': 'coupon.m.jd.com'
 	}
 };
 
+//1.result是基本类型 和 对象 会有什么区别?
+//2.onData应该会在库里给res添加一个引用, onEnd后应该把这个引用去掉?
 var req = http.request(options, (res) => {
-	console.log(`STATUS: ${res.statusCode}`);
-	console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+	// console.log(`STATUS: ${res.statusCode}`);
+	// console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
 	res.setEncoding('utf8');
 	var result = "";
 	res.on('data', (chunk) => {
-		// console.log(`BODY: ${chunk}`);
 		result += chunk;
 	});
 	res.on('end', () => {
-		// console.log('No more data in response.')
-		// console.log(JSON.parse(result));
+		//{a:1,b:2, c:{in1:4, in2:5}}
+
+
+		//{a:1,c:{in1:4}}
+
+		{
+			a: true,
+			c: {
+				in1: true
+			}
+		}
+
 	})
 });
 
@@ -40,4 +58,3 @@ req.on('error', (e) => {
 // write data to request body
 req.write(postData);
 req.end();
-
---
+++ app/tools.js
@@ -0,0 +1,31 @@
+module.exports = {
+	/*
+	@usage: 
+	objFilter({
+		a:1,
+		b:2,
+		c: {
+			c1:3,
+			c2:4
+		}
+	}, {
+		a: true,
+		c: {
+			c1: true
+		}
+	})
+	@result: {a:1, c:{c1:3}}
+	*/
+	objFilter: (srcObj, filter) => {
+		var destObj = {};
+		for (key in filter) {
+			if (filter[key] === true) { //普通值
+				destObj[key] = srcObj[key];
+			} else { // 对象
+				destObj[key] = module.exports.objFilter(srcObj[key], filter[key]);
+			}
+		}
+
+		return destObj;
+	}
+}
